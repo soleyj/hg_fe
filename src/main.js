49de529a6735642d2a2283331c6634fc324f32b1
soleyj <48433045+soleyj@users.noreply.github.com>
2020-06-25 22:37:01 +0200

    add login and some librarays

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
+import 'core-js/stable'
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
 import router from './router'
+import CoreuiVue from '@coreui/vue'
+import { iconsSet as icons } from './assets/icons/icons.js'
 import store from './store'
 
-Vue.config.productionTip = false
+Vue.config.performance = true
+Vue.use(CoreuiVue)
+Vue.prototype.$log = console.log.bind(console)
 
 new Vue({
   router,
   store,
+  icons,
   render: h => h(App)
 }).$mount('#app')

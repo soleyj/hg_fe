cd45573d10c2a2bdd42a8260ece8e2c07ca5d778
soleyj <48433045+soleyj@users.noreply.github.com>
2020-06-26 12:27:18 +0200

    Revert "add login and some librarays"
    This reverts commit 49de529a6735642d2a2283331c6634fc324f32b1.

--- src/main.js
+++ src/main.js
@@ -1,18 +1,12 @@
-import 'core-js/stable'
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router'
-import CoreuiVue from '@coreui/vue'
-import { iconsSet as icons } from './assets/icons/icons.js'
 import store from './store'
 
-Vue.config.performance = true
-Vue.use(CoreuiVue)
-Vue.prototype.$log = console.log.bind(console)
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  icons,
   render: h => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
 
-import Login from '../views/auth/Login.vue'
 Vue.use(VueRouter)
 
   const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Login
+    component: Home
   },
   {
     path: '/about',
